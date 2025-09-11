efa16426299f99677604f4c0f8dc4356fed5b783
Adam Lamouri <adamlamouri@1344.fr>
2025-09-11 22:54:20 +0200

    provider and uid set for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,18 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  before_create :set_uid_and_provider
   after_create :set_username
 
   def set_username
     self.username = "#{first_name.downcase}#{last_name.upcase.first}#{rand(1000..9999)}"
     self.save!
+  end
+
+  def set_uid_and_provider
+    if provider.blank? || uid.blank?
+      self.provider = "email"
+      self.uid = email
+    end
   end
 end
