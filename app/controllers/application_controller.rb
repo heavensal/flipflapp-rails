00c4f0c5683f8484abfb282f0e1f5592925a22d2
Adam Lamouri <adamlamouri@1344.fr>
2025-09-15 22:19:30 +0200

    allow browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # Note: If Safari on iPhone cannot open the site, you may need to adjust the browser compatibility settings.
+  # allow_browser versions: :modern
 end
