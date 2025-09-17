ca17935eb27439153c5cbafa40fac28ddc826bfe
Adam Lamouri <adamlamouri@1344.fr>
2025-09-17 21:47:07 +0200

    better email visibility

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.asset_host = "http://localhost:3000"
+
+
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
 
