50d7d480dfe98e63e4b943b68594b96bca949eb6
adam lamouri <adam.lamouri@epitech.eu>
2026-07-21 01:11:52 +0200

    Refactor index addition in invitations migration for consistency
    - Adjusted the formatting of the index addition in the `CreateInvitations` migration file to ensure consistent spacing.

--- db/migrate/20260720223852_create_invitations.rb
+++ db/migrate/20260720223852_create_invitations.rb
@@ -5,7 +5,6 @@ class CreateInvitations < ActiveRecord::Migration[8.0]
       t.references :user, null: false, foreign_key: true # invited player
       t.timestamps
     end
-    
-    add_index :invitations, [:event_id, :user_id], unique: true
+    add_index :invitations, [ :event_id, :user_id ], unique: true
   end
 end
